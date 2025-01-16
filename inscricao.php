90d7174dc98ce2b735801107b510fc8258d7b5b5
Aula-talento-tech <valquiriarocha001@gmail.com>
2025-01-16 13:51:47 -0300

    Update inscricao.php

--- inscricao.php
+++ inscricao.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Confirmação</title>
-</head>
+<head>
 
 <body>
 
@@ -15,4 +15,4 @@
 
 </body>
 
-</html> 
+<html> 
